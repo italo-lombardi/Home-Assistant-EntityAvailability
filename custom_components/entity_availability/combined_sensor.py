64e12aef5a4c305621e0ef4d0df239022a63dd85
Italo Lombardi <italo.lombardi@sap.com>
2026-08-05 14:49:09 +0100

    fix(combined_sensor): count all NE in g_non_essential regardless of suppression

--- custom_components/entity_availability/combined_sensor.py
+++ custom_components/entity_availability/combined_sensor.py
@@ -426,9 +426,7 @@ class CombinedGroupSensor(CombinedSensorBase):
         registry = er.async_get(self.hass)
         for coord in active:
             states = coord.device_states
-            g_non_essential = sum(
-                1 for d in states.values() if d.is_non_essential and not d.is_suppressed
-            )
+            g_non_essential = sum(1 for d in states.values() if d.is_non_essential)
             g_total = len(coord.monitored_entities) - g_non_essential
             g_offline_entities = [
                 d.entity_id
